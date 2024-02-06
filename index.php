851df7675bbae6b76806b07a73e085580f6d575e
Russell Heimlich <kingkool68@gmail.com>
2024-02-05 22:33:54 -0500

    Make the grid of math problems fill the whole page

--- index.php
+++ index.php
@@ -6,10 +6,20 @@ require_once 'class-twig.php';
 $context = array(
 	'problems' => Mathabunga::get_problems(
 		array(
-			'number_of_problems' => 90,
-			'problem_layout'     => 'vertical',
+			'operations'         => array(
+				'addition'    => array(
+					'min' => 10,
+					'max' => 1000,
+				),
+				'subtraction' => array(
+					'min' => 1,
+					'max' => 12,
+				),
+			),
+			'number_of_problems' => 150,
+			'problem_layout'     => 'horizontal',
 		)
 	),
-	'layout' => 'vertical',
+	'layout'   => 'vertical',
 );
 Twig::out( 'index.twig', $context );
